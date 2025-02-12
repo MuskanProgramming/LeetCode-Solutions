3f332390ddd5a48c55eb097596cd6a74133a00fa
MuskanProgramming <muskanpriya97@gmail.com>
2025-02-12 21:47:36 +0530

    Added Find First And Last Position Of Element In Sorted Array solution

---
+++ ArraysSol/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,50 @@
+/*
+ * Problem Statement - Given an array of integers sorted in non-decreasing order, find the starting and ending position of a given target value.
+ * If the target is not found, return [-1, -1].
+ *
+ * Example 1:
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ *
+ * Example 2:
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ *
+ * Optimized Approach - O(log n) Time, O(1) Space
+ * - Use binary search to find the first and last occurrence.
+ */
+
+ public class FindFirstAndLastPositionOfElementInSortedArray {
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return new int[]{first, last};
+    }
+
+    private static int findBound(int[] nums, int target, boolean isFirst) {
+        int left = 0, right = nums.length - 1;
+        int bound = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                bound = mid;
+                if (isFirst) right = mid - 1;
+                else left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return bound;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] result = searchRange(nums, target);
+        System.out.println("First and Last Position: [" + result[0] + ", " + result[1] + "]");
+    }
+}
